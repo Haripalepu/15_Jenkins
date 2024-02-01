d2faf8b949552017b249eec866800dba315192a9
hari <hari@gmail.com>
2024-02-01 21:47:47 +0530

    adding ec2 file

--- 08_Input/ec2_vars.tf
+++ 08_Input/ec2_vars.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Jenkins_Input" {  
   ami           = "ami-0f3c7d07486cad139" #devops-practice ami
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.Jenkins_input_sg]
+  vpc_security_group_ids = [aws_security_group.Jenkins_input_sg.id]
 
   tags = {
     Name = "Jenkins_Input" #Ec2 Instance name 
